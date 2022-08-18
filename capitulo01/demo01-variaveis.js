a64528d9fbdbffe680983a8a946272d35d04ddf7
Caio Vinicius De Lima <32105582+caiovlima@users.noreply.github.com>
2022-08-18 08:58:01 -0300

    adicionando aulas capítulo 01

---
+++ capitulo01/demo01-variaveis.js
@@ -0,0 +1,16 @@
+// Let pode ser alterado
+let nome = "Super man";
+let idade = 10;
+let poder = "Super força";
+
+// Var não se deve usar mais
+var outro_nome = "Flash";
+
+// const Valores que nunca se alteram
+const segundo_heroi = "Batman";
+
+console.log(
+    `Nome: ${nome}
+    Idade: ${idade}
+    Poder: ${poder}`
+)
---
+++ capitulo01/demo02-strings-e-numbers.js
@@ -0,0 +1,5 @@
+let salarioDoAmigo = 1000;
+let meuSalario = "2999"
+
+console.log(salarioDoAmigo+meuSalario); // "10002999" vai concatenar em forma de string
+console.log(salarioDoAmigo + Number(meuSalario)); // 3999 inteiro
